7f2ede9f588df700b965dc6cddf78496952cd6ff
Michael Saunders <michaelsaunders94@googlemail.com>
2017-03-24 22:18:41 +0000

    Added Fits and Functions: Edit, Update and Destroy

--- app/controllers/fits_controller.rb
+++ app/controllers/fits_controller.rb
@@ -4,12 +4,32 @@ class FitsController < ApplicationController
         @fits = Fit.all
     end
     
+    def show
+        @fit = Fit.find(params[:id])
+    end
+    
     def new
         @fit = Fit.new
     end
 
     def create
         @fit = Fit.create(fit_params)
+        redirect_to fits_path
+    end
+
+    def edit
+        @fit = Fit.find(params[:id])
+    end
+
+    def update
+        @fit = Fit.find(params[:id])
+        @fit.update(fit_params)
+        redirect_to(fit_path(@fit))
+    end
+    
+    def destroy
+        @fit = Fit.find(params[:id])
+        @fit.destroy
         redirect_to fits_path
     end
 
